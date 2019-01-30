9a1abc215e7c966181debed59afe1976d3dbbb56
Nathen Johns <nathenjohns@yahoo.co.uk>
2019-01-30 13:40:49 +0000

    settings to get images from S3 database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 if "DATABASE_URL" in os.environ:
     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 else:
-    print("Database URL not fond. Using SQLite instead")
+    print("Database URL not found. Using SQLite instead")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -152,8 +152,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
